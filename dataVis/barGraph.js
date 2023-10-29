6655f19315beefaf03686c59e19a946fac79241f
AthenaNaidoo <athena.naidoo4r@gmail.com>
2023-10-29 09:08:24 +0200

    dataArt

--- dataVis/barGraph.js
+++ dataVis/barGraph.js
@@ -1,5 +1,23 @@
 console.log(d3);
 
+var exampleURL = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-10-31"
+
+					var apiKey = 'TRB7uN3UkZyKymFv8Ltd9mf5zgnhmz8nwaXPFHVg'; 
+
+					var request = new XMLHttpRequest(); 
+					request.open('GET', exampleURL + '&api_key=' + apiKey, true);
+
+					request.addEventListener('load',function(){
+
+					if(request.status >= 200 && request.status < 400){
+					var response = JSON.parse(request.responseText);
+					console.log(response);
+					} 
+					else {
+					     console.log("Error in network request: " + request.statusText);
+					}});
+					request.send(null);
+
 let margin=50;
 let vizArea = d3.select('#BG');
 let xScale = d3.scaleLinear().domain([0,100]).range([0,500]);
